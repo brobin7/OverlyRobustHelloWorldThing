3c025aeafe20310a4489d4d792dd266b8290de64
brobin7 <brobin7@uic.edu>
2017-02-13 07:57:37 -0600

    Update IDbCommunicator.cs

--- HelloWorld/Interfaces/IDbCommunicator.cs
+++ HelloWorld/Interfaces/IDbCommunicator.cs
@@ -6,7 +6,6 @@ namespace HelloWorld.Interfaces
 {
     public interface IDbCommunicator
     {
-        Task<T> QuerySingleOrDefaultAsync<T>(CommandDefinition command);
         Task<T> QuerySingleAsync<T>(CommandDefinition command);
         Task<IEnumerable<T>> QueryAsync<T>(CommandDefinition command);
         Task<int> ExecuteAsync(CommandDefinition command);
